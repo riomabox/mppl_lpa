61e45b5a57c27c2c45bbde1893764903d4cae1dd
Usaid Chaq <usaidelqudsy.chaq20@gmail.com>
2016-04-17 19:27:04 +0700

    mengembalikan base_url

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,8 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://localhost/lpa/mppl-lpa/';
+$config['base_url'] = 'http://localhost/mppl-lpa/trunk';
+//$config['base_url'] = 'http://localhost/lpa/mppl-lpa/';
 
 /*
 |--------------------------------------------------------------------------
